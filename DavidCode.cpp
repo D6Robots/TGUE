e7e75a1a03bbf91f5f673ffd27b4315dab2ff135
vellazad <vellazad@uni.coventry.ac.uk>
2017-03-17 17:23:27 +0000

    DavidCode - V2

--- DavidCode.cpp
+++ DavidCode.cpp
@@ -14,30 +14,29 @@ class Map
 {
 	public:
     
+    // Takes a text file containing a map as input, and returns an array of arrays, with each array being a row and each element of the array being an element in the game
     array <array<string, 160>, 40> importMap(string fileName)
-    /* Takes a text file containing a map as input, and returns an array of arrays, with each array being a row and each element of the array being an element in the game */
 	{
-		ifstream mapFile;
-		mapFile.open(fileName);  // Opens the text file containing the map for reading
+        ifstream mapFile;  // Creates a file object for reading
+		mapFile.open(fileName);  // Opens the text file containing the map and assigns it to the file object
 		
-		array <array<string, 160>, 40> mapData;  // The array of arrays containing the data for each coordinate on the map
-
-		for (int i = 0; i < 40; i++)  // For every line
+		// Stores every character in the map in an array of arrays, with each array representing a row
+        array <array<string, 160>, 40> mapData;
+        for (int i = 0; i < 40; i++)  // For every line
 		{
-			string lineString;  // Stores the row as a string
+			string lineString;
 			array<string, 160> lineArray;
-			mapFile >> lineString;
+			mapFile >> lineString;  // Stores the row as a string
             
-            for (int j = 0; j < 160; j++)  // Splits the string into characters which will be stored in an array for the row
+            for (int j = 0; j < 160; j++)  // For every character in the line
             {
-                lineArray[j] = lineString.substr(j, 1);
+                lineArray[j] = lineString.substr(j, 1);  // Stores the character as a string in an array
             }
 
 			mapData[i] = lineArray;
-
 		}
-        
-        // Finds and replaces 'o' in the array with ' '
+
+        // Finds and replaces every 'o' in the array with ' '
         for (int i = 0; i < 40; i++)
         {
             array<string, 160> currentLine = mapData[i];
@@ -56,35 +55,38 @@ class Map
 		mapFile.close();
 	}
     
-    // void getHealthPickups(std::string sqliteFile)
-    // {
-    // try
-    // {
-    // sqlite::sqlite db( sqliteFile );
-    // auto cur = db.get_statement();
-    // cur->step();
-    //     cur->set_sql( "select * from Loot where Type = 'health';");
-    // cur->prepare();
-    //    while( cur->step())
-    //    {
-    //        string name = cur ->get_text(1);
-    //        cout << name << endl;
-    //    }
-    // }
-    // catch( sqlite::exception e )      // catch all sql issues
-    // {
-    //     std::cerr << e.what() << std::endl;
-    // }
-    // }
+    /*
+    void getHealthPickups(std::string sqliteFile)
+    {
+        try
+        {
+            sqlite::sqlite db( sqliteFile );
+            auto cur = db.get_statement();
+            cur->step();
+            cur->set_sql( "select * from Loot where Type = 'health';");
+            cur->prepare();
+            while( cur->step())
+            {
+                string name = cur ->get_text(1);
+                cout << name << endl;
+            }
+        }
+        
+        catch( sqlite::exception e )      // catch all sql issues
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+    */
     
-    void spawnPickups(array <array<string, 160>, 40> mapData)
-    /* Takes the map as an array as input and adds pickups at random empty locations before returning the map */
+    // Takes the map as an array as input and adds pickups at random empty locations before returning the map
+    array <array<string, 160>, 40> spawnPickups(array <array<string, 160>, 40> mapData)
     {
         
     }
     
+    // Takes the map as an array as input, and prints it to the screen
     void drawMap(array <array<string, 160>, 40> mapData)
-    /* Takes the map as an array as input, and prints it to the screen */
     {
         for (int i = 0; i < 40; i++)  // For every line
         {
@@ -93,16 +95,19 @@ class Map
             {
                 cout << currentLine[j];
             }
-            cout << endl;
+            cout << endl;  // Moves to the next line after the loop is done with the row
         }
     }
 };
+
 
 class Player
 {
     public:
     
-    /* Constructor assigning health and a starting position to the player.  Checks to make sure starting values are valid */
+    short health;
+    
+    // Constructor assigning health and a starting position to the player.  Checks to make sure starting values are valid 
     Player(short hp)
     {
         if(hp > 100 or hp < 0)
@@ -111,14 +116,14 @@ class Player
         }
         else
         {
-            short health = hp;
+            Player::health = hp;
         }
     }
     
-    /* Moves the player in the specified direction */
+    // Moves the player in the specified direction and returns the updated map
     array<array<string, 160>, 40> move(array<int, 2> currentPosition, string direction, array <array<string, 160>, 40> mapData)
     {
-        // Calculates the next position of the player
+        // Calculates what the next position of the player would be, in order to check it for collisions
         array<int, 2> tentativePosition;
         if (direction == "right")
         {
@@ -137,25 +142,25 @@ class Player
             tentativePosition = {currentPosition[0], currentPosition[1] + 1};
         }
         
-        // Updates map with new position, leaving it unchanged if the position the player chose is invalid
+        // Checks for collisions and updates map accordingly
         if (mapData[tentativePosition[1]][tentativePosition[0]] == "#")
         {
-            return mapData;  // Leaves map unchanged
+            cout << "You hit a wall!" << endl;  // Leaves map unchanged if player collides with a wall
         }
         else if (mapData[tentativePosition[1]][tentativePosition[0]] == "E")
         {
-            mapData[0][0] = "next level";
-            return mapData;
+            mapData[0][0] = "next level";  // Adds a tag which tells the game to go to the next level, if the player reached the end of the level
         }
         else
         {
             mapData[currentPosition[1]][currentPosition[0]] = " ";  // Sets the previous position to empty space
             mapData[tentativePosition[1]][tentativePosition[0]] = "p";  // Puts player in the next position
-            return mapData;
         }
+        
+        return mapData;
     }
     
-    /* Gets the player's current position */
+    // Gets the player's current position
     array<int, 2> getPosition(array <array<string, 160>, 40> mapData)
     {
         array<int, 2> currentPosition;
@@ -173,6 +178,7 @@ class Player
     }
 };
 
+
 int main()
 {
 	
@@ -187,9 +193,11 @@ int main()
     
     short currentLevel = 1;
     
-    /* Main game loop */
+    // Main game loop
     while(gameOver == false)
     {
+        system("clear");
+        
         if (mapData[0][0] == "next level")
         {
             if (currentLevel == 1)
@@ -214,6 +222,7 @@ int main()
             }
             else if (currentLevel == 5)
             {
+                cout << "Congratulations!" << endl;
                 gameOver = true;
             }
         }
@@ -221,6 +230,7 @@ int main()
         currentPosition = player.getPosition(mapData);
         map.drawMap(mapData);
         
+        cout << endl << "Your health is " << player.health << "." << endl;
         cout << "What is your next move?: ";
         cin >> nextMove;
         
@@ -245,7 +255,6 @@ int main()
             gameOver = true;
         }
         
-        system("clear");
     }
     
     
